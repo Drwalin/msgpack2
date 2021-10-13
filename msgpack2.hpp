13b70ee22bd8f385f9cc45ebee4c318e4188f1f8
Drwalin <34726800+Drwalin@users.noreply.github.com>
2021-10-13 23:20:43 +0200

    A lot of modifications in API. Better README.

--- msgpack2.hpp
+++ msgpack2.hpp
@@ -1,5 +1,5 @@
 /*
- *  This file is one header file for msgpack2. Please see README for details.
+ *  This file is part of msgpack2. Please see README for details.
  *  Copyright (C) 2021 Marek Zalewski aka Drwalin
  *
  *  ICon3 is free software: you can redistribute it and/or modify
@@ -27,57 +27,9 @@
 #include <string>
 #include <span>
 
-namespace endian {
-	template<typename T>
-		inline T byteswap(T value) {
-			if constexpr(sizeof(T) == 1)
-				return value;
-			union {
-				T v;
-				uint8_t b[sizeof(T)];
-			} src, dst;
-			src.v = value;
-			if constexpr(sizeof(T) == 2) {
-				dst.b[0] = src.b[1];
-				dst.b[1] = src.b[0];
-				return dst.v;
-			} else if constexpr(sizeof(T) == 4) {
-				dst.b[0] = src.b[3];
-				dst.b[1] = src.b[2];
-				dst.b[2] = src.b[1];
-				dst.b[3] = src.b[0];
-				return dst.v;
-			} else if constexpr(sizeof(T) == 8) {
-				dst.b[0] = src.b[7];
-				dst.b[1] = src.b[6];
-				dst.b[2] = src.b[5];
-				dst.b[3] = src.b[4];
-				dst.b[4] = src.b[3];
-				dst.b[5] = src.b[2];
-				dst.b[6] = src.b[1];
-				dst.b[7] = src.b[0];
-				return dst.v;
-			}
-		}
+#include "endian.hpp"
 
-	template<typename T>
-		inline T ReadBigEndiand(const uint8_t* buffer) {
-			if constexpr (std::endian::native == std::endian::big)
-				return *(const T*)buffer;
-			else
-				return byteswap(*(const T*)buffer);
-		}
-
-	template<typename T>
-		inline void WriteBigEndiand(T value, uint8_t* buffer) {
-			if constexpr (std::endian::native == std::endian::big)
-				*(T*)buffer = value;
-			else
-				*(T*)buffer = byteswap(value);
-		}
-}
-
-namespace ReaderWriter {
+namespace msgpack2 {
 	// uint8[] == int8[] = char[] = string
 	enum Type {
 		_uint7        = 0x00,  // 0x00:0x7F == 0x00:0x7F : [0x80]
@@ -90,7 +42,7 @@ namespace ReaderWriter {
 		_array_types_begin = 0xF0,
 
 		_string_long  = 0xF0,  // [ int size, uint8[size+0x40]      ]
-		_array_long   = 0xF1,  // [ int size, any[size+0x1F]        ]
+		_array_long   = 0xF1,  // [ int size, any[size+0x20]        ]
 		_map_long     = 0xF2,  // [ int size, {any ,any}[size+0x10] ]
 
 		_array_uint16  = 0xF3,  // [ int size, uint16[size]      ]
@@ -122,426 +74,89 @@ namespace ReaderWriter {
 
 
 	inline Type GetType(const uint8_t* buffer);
+	inline void ReadTypeAndSize(Type& type, uint64_t& size,
+			const uint8_t*& buffer, const uint8_t* end);
+	
+	inline const uint64_t ReadInteger(const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(uint8_t& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(uint16_t& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(uint32_t& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(uint64_t& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(int8_t& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(int16_t& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(int32_t& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(int64_t& value, const uint8_t*& buffer, const uint8_t* end);
+	
+	inline void Write(bool value, std::vector<uint8_t>& buffer);
+	
+	inline std::string_view ReadString(const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(std::string_view& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(const char** value, uint64_t& size, const uint8_t*& buffer, const uint8_t* end);
+	
+	inline long double ReadReal(const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(long double& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(double& value, const uint8_t*& buffer, const uint8_t* end);
+	inline void Read(float& value, const uint8_t*& buffer, const uint8_t* end);
+	
+	inline bool ReadArrayHeader(uint64_t& size, const uint8_t*& buffer, const uint8_t* end);
+	inline bool ReadMapHeader(uint64_t& size, const uint8_t*& buffer, const uint8_t* end);
+	// TODO make Readers for arrays and maps
+	// TODO make Readers for typed arrays
+	
+	
 
-	const uint8_t* ReadTypeAndSize(Type& type, uint64_t& size,
-			const uint8_t* buffer, const uint8_t* end);
 
+	inline void WriteInteger(int64_t value, std::vector<uint8_t>& buffer);
+	inline void Write(uint8_t value, std::vector<uint8_t>& buffer);
+	inline void Write(uint16_t value, std::vector<uint8_t>& buffer);
+	inline void Write(uint32_t value, std::vector<uint8_t>& buffer);
+	inline void Write(uint64_t value, std::vector<uint8_t>& buffer);
+	inline void Write(int8_t value, std::vector<uint8_t>& buffer);
+	inline void Write(int16_t value, std::vector<uint8_t>& buffer);
+	inline void Write(int32_t value, std::vector<uint8_t>& buffer);
+	inline void Write(int64_t value, std::vector<uint8_t>& buffer);
+	
+	inline void Write(bool value, std::vector<uint8_t>& buffer);
+
+	inline void WriteString(std::string_view value, std::vector<uint8_t>& buffer);
+	inline void Write(std::string_view value, std::vector<uint8_t>& buffer);
+	
+	inline void WriteReal(long double value, std::vector<uint8_t>& buffer);
+	inline void Write(long double value, std::vector<uint8_t>& buffer);
+	inline void Write(double value, std::vector<uint8_t>& buffer);
+	inline void Write(float value, std::vector<uint8_t>& buffer);
+	
 	template<typename T>
-		inline const uint8_t* Read(T& value, const uint8_t* buffer, const uint8_t* end);
-
-	inline const uint8_t* ReadString(char**const str, size_t& size,
-			const uint8_t* buffer, const uint8_t* end);
-
-
-	inline void InitArray(Type type, uint64_t elements,
-			std::vector<uint8_t, allocator_any<uint8_t>>& buffer);
-
-	inline void InitMap(uint64_t elements,
-			std::vector<uint8_t, allocator_any<uint8_t>>& buffer);
-
+		inline void WriteIntegerSpan(const std::span<T>& value,
+				std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<uint8_t>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<uint16_t>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<uint32_t>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<uint64_t>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<int8_t>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<int16_t>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<int32_t>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<int64_t>& value, std::vector<uint8_t>& buffer);
+	
+	
+	
+	inline void InitArray(uint64_t elements, std::vector<uint8_t>& buffer);
+	inline void InitMap(uint64_t elements, std::vector<uint8_t>& buffer);
+	
 	template<typename T>
-		inline void Write(T value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer);
-
-
-
-	template<>
-		inline const uint8_t* Read<uint64_t>(uint64_t& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			if(buffer >= end)
-				return NULL;
-			uint8_t v = *buffer;
-			size_t mod;
-			switch(v) {
-				case _uint16:
-					value = endian::ReadBigEndiand<uint16_t>(buffer+1)+0x80;
-					mod = 3;
-				case _uint32:
-					value = endian::ReadBigEndiand<uint32_t>(buffer+1)+0x80;
-					mod = 5;
-				case _uint64:
-					value = endian::ReadBigEndiand<uint64_t>(buffer+1)+0x80;
-					mod = 9;
-				case _sint8:
-					value = -*(int8_t*)(buffer+1);
-					mod = 2;
-				case _sint16:
-					value = -endian::ReadBigEndiand<int16_t>(buffer+1);
-					mod = 3;
-				case _sint32:
-					value = -endian::ReadBigEndiand<int32_t>(buffer+1);
-					mod = 5;
-				case _sint64:
-					value = -endian::ReadBigEndiand<int64_t>(buffer+1);
-					mod = 9;
-				default:
-					if(v >= _uint7) {
-						value = v - _uint7;
-						mod = 1;
-					} else {
-						value = 0;
-						return NULL;
-					}
-			}
-			buffer += mod;
-			if(buffer > end)
-				return NULL;
-			return buffer;
-		}
-
-	template<>
-		inline const uint8_t* Read<uint8_t>(uint8_t& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			uint64_t v64=0;
-			buffer = Read<uint64_t>(v64, buffer, end);
-			value = v64;
-			return buffer;
-		}
-
-	template<>
-		inline const uint8_t* Read<uint16_t>(uint16_t& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			uint64_t v64=0;
-			buffer = Read<uint64_t>(v64, buffer, end);
-			value = v64;
-			return buffer;
-		}
-
-	template<>
-		inline const uint8_t* Read<uint32_t>(uint32_t& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			uint64_t v64=0;
-			buffer = Read<uint64_t>(v64, buffer, end);
-			value = v64;
-			return buffer;
-		}
-
-	template<>
-		inline const uint8_t* Read<int8_t>(int8_t& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			uint64_t v64=0;
-			buffer = Read<uint64_t>(v64, buffer, end);
-			value = v64;
-			return buffer;
-		}
-
-	template<>
-		inline const uint8_t* Read<int16_t>(int16_t& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			uint64_t v64=0;
-			buffer = Read<uint64_t>(v64, buffer, end);
-			value = v64;
-			return buffer;
-		}
-
-	template<>
-		inline const uint8_t* Read<int32_t>(int32_t& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			uint64_t v64=0;
-			buffer = Read<uint64_t>(v64, buffer, end);
-			value = v64;
-			return buffer;
-		}
-
-	template<>
-		inline const uint8_t* Read<int64_t>(int64_t& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			return Read<uint64_t>((uint64_t&)value, buffer, end);
-		}
-
-	template<>
-		inline const uint8_t* Read<bool>(bool& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			if(*buffer == _true)
-				value = true;
-			else if(*buffer == _false)
-				value = false;
-			else
-				return NULL;
-			return buffer+1;
-		}
-
-	template<>
-		inline const uint8_t* Read<long double>(long double& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			value = 0;
-			if(buffer >= end)
-				return NULL;
-			if(*buffer == _real) {
-				int64_t exponential=0, mantissa=0;
-				const uint8_t* e = Read<int64_t>(mantissa, buffer+1, end);
-				const uint8_t* e2 = Read<int64_t>(exponential, e, end);
-				if(e2 > end)
-					return NULL;
-				value = mantissa;
-				value = ldexpl(mantissa, exponential);
-				return e2;
-			}
-			return NULL;
-		}
-
-	template<>
-		inline const uint8_t* Read<double>(double& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			long double v=0;
-			buffer = Read<long double>(v, buffer, end);
-			value = v;
-			return buffer;
-		}
-
-	template<>
-		inline const uint8_t* Read<float>(float& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			long double v=0;
-			buffer = Read<long double>(v, buffer, end);
-			value = v;
-			return buffer;
-		}
-
-
-
-	template<>
-		inline const uint8_t* Read<std::string_view>(std::string_view& value,
-				const uint8_t* buffer, const uint8_t* end) {
-			size_t size;
-			const char* str = NULL;
-			buffer = ReadString((char**const)&str, size, buffer, end);
-			if(buffer) {
-				value = std::string_view((const char*)buffer,
-						(const char*)buffer+size);
-				return buffer;
-			}
-			return NULL;
-		}
-
-	inline const uint8_t* ReadString(char**const str, size_t& size,
-			const uint8_t* buffer, const uint8_t* end) {
-		Type type;
-		buffer = ReadTypeAndSize(type, size, buffer, end);
-		if(type == _string_long || type == _string_short) {
-			*str = (char*const)buffer;
-			buffer += size;
-			if(buffer > end)
-				return NULL;
-			return buffer;
-		} else {
-			*str = NULL;
-		}
-		return NULL;
-	}
-
-
-
-
-
-
-	inline Type GetType(const uint8_t* buffer) {
-		uint8_t v = *buffer;
-		if(v >= _one_type_types)
-			return (Type)v;
-		else if(v >= _map_short)
-			return _map_short;
-		else if(v >= _array_short)
-			return _array_short;
-		else if(v >= _string_short)
-			return _string_short;
-		else
-			return _uint7;
-	}
-
-	inline const uint8_t* ReadTypeAndSize(Type& type, uint64_t size,
-			const uint8_t* buffer, const uint8_t* end) {
-		uint8_t v = *buffer;
-		if(v >= _one_type_types) {
-			type = (Type)v;
-			buffer++;
-			if(v <= _array_types_end) {
-				buffer = Read<uint64_t>(size, buffer, end);
-				switch(type) {
-					case _string_long:
-						size += _string_short_size;
-						break;
-					case _map_long:
-						size += _map_short_size;
-						break;
-					case _array_long:
-						size += _array_short_size;
-						break;
-					default:
-						break;
-				}
-			} else {
-				size = 0;
-			}
-		} else {
-			buffer++;
-			if(v >= _map_short) {
-				type = _map_short;
-			} else if(v >= _array_short) {
-				type = _array_short;
-			} else if(v >= _string_short) {
-				type = _string_short;
-			} else {
-				type = _uint7;
-				if(buffer > end)
-					return NULL;
-				size = 0;
-				return buffer;
-			}
-			size = v - type;
-		}
-		if(buffer > end)
-			return NULL;
-		return buffer;
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-	template<>
-		inline void Write<int64_t>(int64_t value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			if(value < 0) {
-				value = -value;
-				if(value < 0x100) {
-					buffer.emplace_back(_sint8);
-					buffer.emplace_back(value);
-				} else if(value < 0x10000) {
-					buffer.emplace_back(_sint16);
-					buffer.resize(buffer.size()+2);
-					endian::WriteBigEndiand<uint16_t>(value,
-							buffer.data()+buffer.size()-2);
-				} else if(value < 0x10000000) {
-					buffer.emplace_back(_sint32);
-					buffer.resize(buffer.size()+4);
-					endian::WriteBigEndiand<uint64_t>(value,
-							buffer.data()+buffer.size()-4);
-				} else {
-					buffer.emplace_back(_sint64);
-					buffer.resize(buffer.size()+8);
-					endian::WriteBigEndiand<uint64_t>(value,
-							buffer.data()+buffer.size()-8);
-				}
-			} else {
-				if(value <= _uint7_max) {
-					buffer.emplace_back(value + _uint7);
-				} else {
-					value -= _uint7_max+1;
-					if(value < 0x10000) {
-						buffer.emplace_back(_uint16);
-						buffer.resize(buffer.size()+2);
-						endian::WriteBigEndiand<uint16_t>(value,
-								buffer.data()+buffer.size()-2);
-					} else if(value < 0x10000000) {
-						buffer.emplace_back(_uint32);
-						buffer.resize(buffer.size()+4);
-						endian::WriteBigEndiand<uint64_t>(value,
-								buffer.data()+buffer.size()-4);
-					} else {
-						buffer.emplace_back(_uint64);
-						buffer.resize(buffer.size()+8);
-						endian::WriteBigEndiand<uint64_t>(value,
-								buffer.data()+buffer.size()-8);
-					}
-				}
-			}
-		}
-
-	template<>
-		inline void Write<int32_t>(int32_t value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			Write<int64_t>(value, buffer);
-		}
-
-	template<>
-		inline void Write<int16_t>(int16_t value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			Write<int64_t>(value, buffer);
-		}
-
-	template<>
-		inline void Write<int8_t>(int8_t value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			Write<int64_t>(value, buffer);
-		}
-
-	template<>
-		inline void Write<uint8_t>(uint8_t value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			Write<int64_t>(value, buffer);
-		}
-
-	template<>
-		inline void Write<uint16_t>(uint16_t value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			Write<int64_t>(value, buffer);
-		}
-
-	template<>
-		inline void Write<uint32_t>(uint32_t value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			Write<int64_t>(value, buffer);
-		}
-
-	template<>
-		inline void Write<uint64_t>(uint64_t value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			Write<int64_t>(value, buffer);
-		}
-
-	template<>
-		inline void Write<bool>(bool value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			buffer.emplace_back(value ? _true : _false);
-		}
-
-	template<>
-		inline void Write<const std::string&>(const std::string& value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			if(value.size() < _string_short_size)
-				buffer.emplace_back(_string_short + value.size());
-			else
-				Write<uint64_t>(value.size()-_string_short_size, buffer);
-			buffer.insert(buffer.end(), value.begin(), value.end());
-		}
-
-	template<>
-		inline void Write<const std::string_view&>(const std::string_view& value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-			if(value.size() < _string_short_size)
-				buffer.emplace_back(_string_short + value.size());
-			else
-				Write<uint64_t>(value.size()-_string_short_size, buffer);
-			buffer.insert(buffer.end(), value.begin(), value.end());
-		}
-
-
-	inline void InitMap(uint64_t elements,
-			std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-
-	}
-
-	inline void InitArray(Type type, uint64_t elements,
-			std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-
-	}
-
-	template<>
-		inline void Write<const std::span<uint8_t>&>(
-				const std::span<uint8_t>& value,
-				std::vector<uint8_t, allocator_any<uint8_t>>& buffer) {
-
-
-		}
+		inline void WriteRealSpan(const std::span<T>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<long double>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<double>& value, std::vector<uint8_t>& buffer);
+	inline void Write(const std::span<float>& value, std::vector<uint8_t>& buffer);
 }
+
+#ifndef MSGPACK2_READER_INL_CPP
+#include"msgpack2_reader.inl.cpp"
+#endif
+
+#ifndef MSGPACK2_WRITER_INL_CPP
+#include"msgpack2_writer.inl.cpp"
+#endif
 
 #endif
 
